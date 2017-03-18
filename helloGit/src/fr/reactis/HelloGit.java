e87fc973c56096144bb580f4f448d246e10c1152
Mak <Makhlouf.BOURAHLA@POR-2017-003.REACTIS.LOCAL>
2017-03-18 15:22:25 +0100

    converted for loop

--- helloGit/src/fr/reactis/HelloGit.java
+++ helloGit/src/fr/reactis/HelloGit.java
@@ -5,10 +5,10 @@ public class HelloGit {
 	public static void main(String[] args) {
 		
 		String[] names = {"toto1","toto2","toto3","toto4","toto5","toto6"};
-		//ceci est un commentaire
-		for (int i = 0; i < names.length; i++) {
-			System.out.format("Hello %s! \n......");			
-		}
+		//ceci est un commentaire more comment here........
+		for (String string : names) {
+			System.out.format("Hello %s! \n......");						
+		} 
 	}
 
 }
