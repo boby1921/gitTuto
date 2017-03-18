ad831d0aae2dd3f609da81acac3614b054af4d61
Mak <Makhlouf.BOURAHLA@POR-2017-003.REACTIS.LOCAL>
2017-03-18 15:11:57 +0100

    mettre à jour

--- helloGit/src/fr/reactis/HelloGit.java
+++ helloGit/src/fr/reactis/HelloGit.java
@@ -3,8 +3,12 @@ package fr.reactis;
 public class HelloGit {
 	
 	public static void main(String[] args) {
+		
+		String[] names = {"toto1","toto2","toto3","toto4","toto5","toto6"};
 		//ceci est un commentaire
-		System.out.println("Hello from git......");
+		for (int i = 0; i < names.length; i++) {
+			System.out.format("Hello %s! \n......");			
+		}
 	}
 
 }
