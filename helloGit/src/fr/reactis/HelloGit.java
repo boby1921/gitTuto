f9f0b080813616b0f3040647269022126b4f5236
Mak2 <Makhlouf.BOURAHLA@POR-2017-003.REACTIS.LOCAL>
2017-03-18 15:24:52 +0100

    added names and sysout

--- helloGit/src/fr/reactis/HelloGit.java
+++ helloGit/src/fr/reactis/HelloGit.java
@@ -4,10 +4,11 @@ public class HelloGit {
 	
 	public static void main(String[] args) {
 		
-		String[] names = {"toto1","toto2","toto3","toto4","toto5","toto6"};
+		String[] names = {"toto1","toto2","toto3","toto4","toto5","toto6","toto7","toto8"};
 		//ceci est un commentaire
 		for (int i = 0; i < names.length; i++) {
-			System.out.format("Hello %s! \n......");			
+			System.out.format("Hello %s! \n......");	
+			System.out.println("how are you doing today....");
 		}
 	}
 
