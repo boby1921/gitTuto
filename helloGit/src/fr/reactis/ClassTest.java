06457b0e77d6530189b15d54eb36cb8a117eace6
Mak <Makhlouf.BOURAHLA@POR-2017-003.REACTIS.LOCAL>
2017-03-20 08:04:38 +0100

    commit new class

---
+++ helloGit/src/fr/reactis/ClassTest.java
@@ -0,0 +1,36 @@
+package fr.reactis;
+
+public class ClassTest {
+
+	private String name;
+	private String prenom;
+	private Long age;
+
+	public ClassTest() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public Long getAge() {
+		return age;
+	}
+
+	public void setAge(Long age) {
+		this.age = age;
+	}
+
+}
